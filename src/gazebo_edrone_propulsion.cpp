973cb91ffea5de3e8a998e510ccfd595cd8d1661
smit <smit@abc>
2020-11-26 11:21:34 +0530

    Version 0.3.0
    For task 3

--- src/gazebo_edrone_propulsion.cpp
+++ src/gazebo_edrone_propulsion.cpp
@@ -184,7 +184,7 @@ namespace gazebo
       jointController->SetVelocityTarget(this->prop3_name, 0.0);
       jointController->SetVelocityTarget(this->prop4_name, 0.0);
     }
-    ROS_INFO("Vitarana Drone version 0.2.2");
+    ROS_INFO("Vitarana Drone version 0.3.0");
     
   }
 
